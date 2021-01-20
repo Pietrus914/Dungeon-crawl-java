32d062578f4ecc1203a61821a089a3fc58853a00
Joanna Rzeszotko-Doniec <rivienne8@gmail.com>
2021-01-20 19:09:29 +0100

    cleaning imports

--- src/main/java/com/codecool/dungeoncrawl/gui/guiControllers/ButtonPickUp.java
+++ src/main/java/com/codecool/dungeoncrawl/gui/guiControllers/ButtonPickUp.java
@@ -1,27 +1,14 @@
 package com.codecool.dungeoncrawl.gui.guiControllers;
 
-import com.codecool.dungeoncrawl.Tiles;
-import com.codecool.dungeoncrawl.gui.StatusLine;
+
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CurrentStatus;
 import com.codecool.dungeoncrawl.logic.GameMap;
 import com.codecool.dungeoncrawl.logic.InventoryBoxDisplayer;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.items.Item;
-import com.codecool.dungeoncrawl.logic.items.ItemNames;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-
-import java.util.ArrayList;
 
 public class ButtonPickUp extends Button {
     private Player player;
